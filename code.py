9a9a43eedf0dbe20d1fbad77b38f1bbfa687dae9
gita006 <geetasurti309@gmail.com>
2025-07-28 03:38:55 +0530

    Update code.py

--- code.py
+++ code.py
@@ -23,22 +23,6 @@ print(X)
 print(Y)
 
 
-# Calculate avarage of each features for all classes
-Y_Data = np.array([np.average(X[:, i][Y==j].astype('float32')) for i in range (X.shape[1]) for j in (np.unique(Y))])
-Y_Data_reshaped = Y_Data.reshape(4, 3)
-Y_Data_reshaped = np.swapaxes(Y_Data_reshaped, 0, 1)
-X_axis = np.arange(len(columns)-1)
-width = 0.25
-
-# Plot the avarage
-plt.bar(X_axis, Y_Data_reshaped[0], width, label = 'Setosa')
-plt.bar(X_axis+width, Y_Data_reshaped[1], width, label = 'Versicolour')
-plt.bar(X_axis+width*2, Y_Data_reshaped[2], width, label = 'Virginica')
-plt.xticks(X_axis, columns[:4])
-plt.xlabel("Features")
-plt.ylabel("Value in cm.")
-plt.legend(bbox_to_anchor=(1.3,1))
-plt.show()
 
 # Split the data to train and test dataset.
 from sklearn.model_selection import train_test_split
